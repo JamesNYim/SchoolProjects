4480be5a71ac53ec64565f8dca073f6b0da32965
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2024-01-18 18:12:19 -0800

    finished game

--- cmpm120/RocketPatrol/src/scenes/Menu.js
+++ cmpm120/RocketPatrol/src/scenes/Menu.js
@@ -64,7 +64,31 @@ class Menu extends Phaser.Scene {
 			'Press <- for Novice  or -> for Expert',
 			menuConfig)
 			.setOrigin(0.5);
-		
-		
+
+		// define keys
+		keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+		keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
 	}
+
+	
+	update() {
+		if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+		  // easy mode
+		  game.settings = {
+			spaceshipSpeed: 3,
+			gameTimer: 60000    
+		  }
+		  this.sound.play('sfx-select')
+		  this.scene.start('playScene')    
+		}
+		if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+		  // hard mode
+		  game.settings = {
+			spaceshipSpeed: 4,
+			gameTimer: 45000    
+		  }
+		  this.sound.play('sfx-select')
+		  this.scene.start('playScene')    
+		}
+	  }
 }
--- cmpm120/RocketPatrol/src/scenes/Play.js
+++ cmpm120/RocketPatrol/src/scenes/Play.js
@@ -117,9 +117,9 @@ class Play extends Phaser.Scene {
 		// GAME OVER 
 		this.gameOver = false
 		scoreConfig.fixedWidth = 0
-		this.clock = this.time.delayedCall(60000, () => {
+		this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
 			this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5)
-			this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5)
+			this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5)
 			this.gameOver = true
 		}, null, this)
 	}
@@ -129,6 +129,9 @@ class Play extends Phaser.Scene {
 		if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyRESET)) {
 			this.scene.restart()
 		}
+		if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+			this.scene.start("menuScene")
+		  }
 
 		this.starfield.tilePositionX -= 4;
 		if(!this.gameOver) {               
