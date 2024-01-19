d5ed4b94c9c5ead6f4cb8ff27e037a806ddc0a97
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2024-01-18 16:12:17 -0800

    Added assets

--- cmpm120/RocketPatrol/src/scenes/Play.js
+++ cmpm120/RocketPatrol/src/scenes/Play.js
@@ -4,6 +4,39 @@ class Play extends Phaser.Scene {
 	}
 	
 	create() {
-	  this.add.text(20, 20, "Rocket Patrol Play");
+	  const greenColor = 0x00FF00;
+	  const whiteColor = 0xFFFFFF;
+
+	  // green UI background
+this.add.rectangle(0, 
+	borderUISize + borderPadding, 
+	game.config.width, borderUISize * 2, 
+	greenColor)
+	.setOrigin(0, 0)
+// white borders
+this.add.rectangle(0,
+	 0, 
+	 game.config.width, 
+	 borderUISize, 
+	 whiteColor)
+	 .setOrigin(0, 0)
+this.add.rectangle(0, 
+	game.config.height - borderUISize, 
+	game.config.width, 
+	borderUISize, 
+	whiteColor)
+	.setOrigin(0, 0)
+this.add.rectangle(0, 
+	0, 
+	borderUISize, 
+	game.config.height, 
+	whiteColor)
+	.setOrigin(0, 0);
+this.add.rectangle(game.config.width - borderUISize,
+	0, 
+	borderUISize, 
+	game.config.height, 
+	whiteColor)
+	.setOrigin(0, 0)
 	}
   }
