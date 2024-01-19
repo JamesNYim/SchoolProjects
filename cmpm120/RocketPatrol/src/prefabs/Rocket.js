9549d19d996452bfc4ed520651cbab96f8e2704d
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2024-01-18 18:02:50 -0800

    Added sound and menu UI

--- cmpm120/RocketPatrol/src/prefabs/Rocket.js
+++ cmpm120/RocketPatrol/src/prefabs/Rocket.js
@@ -5,6 +5,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
 		scene.add.existing(this);
 		this.isFiring = false;
 		this.moveSpeed = 2;
+		this.sfxShot = scene.sound.add('sfx-shot')
 	}
 
 	update() {
@@ -19,8 +20,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
 		}
 
 		// Shooting
-		if (Phaser.Input.Keyboard.JustDown(keyFIRE)) {
+		if (Phaser.Input.Keyboard.JustDown(keyFIRE) && !this.isFiring) {
 			this.isFiring = true;
+			this.sfxShot.play()
 		}
 		if (this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
 			this.y -= this.moveSpeed;
